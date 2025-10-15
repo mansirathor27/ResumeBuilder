208ea2ed4ab791e3f009394d649074b2bc5e65c6
mansirathor27 <mansirathor575@gmail.com>
2025-10-15 07:42:49 +0530

    feat: added create, upload, edit, and delete resume modals with UI improvements and navigation fixes in Dashboard

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Navbar = () => {
     const user = {name: 'Mansi Rathor'}
+    const navigate = useNavigate()
+    const logoutUser=()=>{
+        navigate('/')
+    }
   return (
     <div className='shadow bg-white'>
         <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
             <Link to="/">
                 <img src="/logo.svg" alt=""/>
             </Link>
-            <div>
-                <p>Hi, {user?.name}</p>
-                <button className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>
+            <div className='flex items-center gap-4 text-sm'>
+                <p className='max-sm:hidden'>Hi, {user?.name}</p>
+                <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>
                     Logout
                 </button>
             </div>
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 const Layout = () => {
   return (
     <div>
-      {/* Layout Page */}
-      <h1>Layout Page</h1>
-      <div>
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar/>
         <Outlet/>
       </div>
     </div>
